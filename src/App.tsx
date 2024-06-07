1cdf914bbfa63946c92c8d73e5cfa66630f6c71f
tkaixiang <tkaixiang@gmail.com>
2024-06-07 16:28:17 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import "./App.css";
+import "@fontsource-variable/inter"; // Defaults to weight 400
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <></>;
 }
 
